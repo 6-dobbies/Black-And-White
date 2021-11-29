af8f189b70b897fda580a73180609ecc509354ac
nhk <dmgjgj53@gmail.com>
2021-11-29 17:36:59 +0900

    [test commit]

---
+++ black_and_white/src/main/java/kr/pe/playdata/BlackAndWhiteApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.playdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackAndWhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackAndWhiteApplication.class, args);
+	}
+
+}
