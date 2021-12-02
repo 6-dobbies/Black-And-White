7605cf700e505c9a14220b6f41a87b95838bd510
kwon <ptkeb46@gmail.com>
2021-12-02 16:33:08 +0900

    [test]encode change

--- src/main/java/kr/pe/playdata/controller/MemberController.java
+++ src/main/java/kr/pe/playdata/controller/MemberController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000") // CORS때문에 교차출처에 표시
 @RequiredArgsConstructor
 @RestController
+// @RequestMapping("/con") // /mcon/method 
 public class MemberController {
-
+	
 }
