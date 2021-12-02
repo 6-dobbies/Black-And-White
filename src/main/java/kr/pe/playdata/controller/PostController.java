469d398664bf57ffaf9e3eb36094f886d97df954
YoojinMa <geronimo9595@naver.com>
2021-12-02 15:29:49 +0900

    [Add] controller, service, repository

---
+++ src/main/java/kr/pe/playdata/controller/PostController.java
@@ -0,0 +1,13 @@
+package kr.pe.playdata.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+
+}
