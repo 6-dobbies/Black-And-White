6df4049baa8ea8ba6f55e6da7ddbc52145878e44
kwon <ptkeb46@gmail.com>
2021-12-03 18:14:51 +0900

    [Update] Post

--- src/main/java/kr/pe/playdata/model/dto/ResponseDTO.java
+++ src/main/java/kr/pe/playdata/model/dto/ResponseDTO.java
@@ -1,6 +1,7 @@
 package kr.pe.playdata.model.dto;
 
 import kr.pe.playdata.model.domain.Member;
+import kr.pe.playdata.model.domain.Post;
 import lombok.Getter;
 
 public class ResponseDTO {
@@ -29,5 +30,74 @@ public class ResponseDTO {
             this.region = entity.getRegion();
         }
     }
+	
+	@Getter
+	public static class MemberListResponse {
+		private Long memberIdx;
+		private String memberId;
+		private String pw;
+		private String pwQuestion;
+		private String pwAnswer;
+		private String nickname;
+		private String birthYear;
+		private String email;
+		private String gender;
+		private String region;
+		private String tier;
+		private String role;
+		private int del;
+		
+		public MemberListResponse(Member entity) {
+			this.memberIdx = entity.getMemberIdx();
+			this.memberId = entity.getMemberId();
+			this.pw = entity.getPw();
+			this.pwQuestion = entity.getPwQuestion();
+			this.pwAnswer = entity.getPwAnswer();
+			this.nickname = entity.getNickname();
+			this.birthYear = entity.getBirthYear();
+			this.email = entity.getEmail();
+			this.gender = entity.getGender();
+			this.region = entity.getRegion();
+			this.tier = entity.getTier();
+			this.role = entity.getRole();
+			this.del = entity.getDel();
+		}
+	}
+	
+	@Getter
+    public static class PostResponse {
+		private Long memberIdx;
+		private Long postIdx;
+		private String category;
+		private String title;
+		private String content;
+		private String postImage;
+		
+		public PostResponse(Post entity) {
+			this.memberIdx = entity.getWriter().getMemberIdx();
+			this.postIdx = entity.getPostIdx();
+			this.category = entity.getCategory().getCategory();
+			this.title = entity.getTitle();
+			this.content = entity.getContent();
+			this.postImage = entity.getPostImage();
+		}
+	}
+	
+	@Getter
+	public static class PostListResponse {
+		private Long memberIdx;
+		private Long postIdx;
+		private String title;
+		private String content;
+		private String postImage;
+			
+		public PostListResponse(Post entity) {
+			this.memberIdx = entity.getWriter().getMemberIdx();
+			this.postIdx = entity.getPostIdx();
+			this.title = entity.getTitle();
+			this.content = entity.getContent();
+			this.postImage = entity.getPostImage();
+		}
+	}
 	
 }
