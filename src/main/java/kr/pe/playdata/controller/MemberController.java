fd5dea95d21241af0abd195103b2dfcbd9a855b5
GERONIMO <geronimo9595@naver.com>
2021-12-03 15:01:15 +0900

    [Update] memberController

--- src/main/java/kr/pe/playdata/controller/MemberController.java
+++ src/main/java/kr/pe/playdata/controller/MemberController.java
@@ -1,13 +1,47 @@
 package kr.pe.playdata.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.pe.playdata.model.dto.ResponseDTO;
+import kr.pe.playdata.service.MemberService;
 import lombok.RequiredArgsConstructor;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
-
+	
+	private final MemberService memberService;
+	
+	// 회원 한 명 조회 - memberIdx
+	@GetMapping("/member/idx/{memberIdx}")
+	public ResponseDTO.MemberResponse getMemberOneIdx(@PathVariable Long memberIdx) {
+		return memberService.findByMemberIdx(memberIdx);
+	}
+	
+	// 회원 한 명 조회 - nickname
+	@GetMapping("/member/nickname/{nickname}")
+	public ResponseDTO.MemberResponse getMemberOneNickname(@PathVariable String nickname) {
+		return memberService.findByNickname(nickname);
+	}
+	
+	// 회원 한 명 조회 - nickname 일부
+	
+	
+	// 회원 전체 조회
+	@GetMapping("/member/all")
+	public List<ResponseDTO.MemberListResponse> getMemberAll() {
+		return memberService.findMemberAll();
+	}
+	
+	// 회원 
+	
+	
+	
+	
 }
--- src/main/java/kr/pe/playdata/model/dto/ResponseDTO.java
+++ src/main/java/kr/pe/playdata/model/dto/ResponseDTO.java
@@ -30,4 +30,37 @@ public class ResponseDTO {
         }
     }
 	
+	@Getter
+	public static class MemberListResponse {
+		private Long memberIdx;
+		private String memberId;
+		private String pw;
+		private String pwQuestion;
+		private String pwAnswer;
+		private String nickname;
+		private String birthYear;
+		private String email;
+		private String gender;
+		private String region;
+		private String tier;
+		private String role;
+		private int out;
+		
+		public MemberListResponse(Member entity) {
+			this.memberIdx = entity.getMemberIdx();
+			this.memberId = entity.getMemberId();
+			this.pw = entity.getPw();
+			this.pwQuestion = entity.getPwQuestion();
+			this.pwAnswer = entity.getPwAnswer();
+			this.nickname = entity.getNickname();
+			this.birthYear = entity.getBirthYear();
+			this.email = entity.getEmail();
+			this.gender = entity.getGender();
+			this.region = entity.getRegion();
+			this.tier = entity.getTier();
+			this.role = entity.getRole();
+			this.out = entity.getOut();
+		}
+	}
+	
 }
