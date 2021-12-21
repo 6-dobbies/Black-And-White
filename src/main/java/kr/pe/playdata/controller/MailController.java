d26699b03ddeb24755b1297a8debdc037db3c325
GERONIMO <geronimo9595@naver.com>
2021-12-22 05:20:00 +0900

    [Fix] login, mail

--- src/main/java/kr/pe/playdata/controller/MailController.java
+++ src/main/java/kr/pe/playdata/controller/MailController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.pe.playdata.model.dto.MemberDTO;
+import kr.pe.playdata.model.dto.ResponseDTO;
 import kr.pe.playdata.model.response.SingleResult;
 import kr.pe.playdata.service.MailService;
 import kr.pe.playdata.service.MemberService;
@@ -24,10 +25,10 @@ public class MailController {
 	@PatchMapping("/email")
 	public SingleResult<String> saveLocation(@RequestBody MemberDTO.TempoPw dto) {
 
-		String email = memberService.tempoPw(dto.getMemberId(), dto.getPwQuestion(), dto.getPwAnswer());
+		ResponseDTO.MemberResponse entity = memberService.tempoPw(dto.getMemberId(), dto.getPwQuestion(), dto.getPwAnswer());
 
-		if (email != "false") {
-			mailService.sendEmail(email, "임시 비밀번호 안내 메일입니다:)", "임시로 발급된 비밀번호는 " + dto.getPw() + "입니다.");
+		if (entity != null) {
+			mailService.sendEmail(entity.getEmail(), "임시 비밀번호 안내 메일입니다:)", "임시로 발급된 비밀번호는 " + entity.getPw() + "입니다.");
 			return responseService.getSingleResult("메일이 발송되었습니다");
 		} else {
 			return responseService.getSingleResult("아이디 또는 질문 또는 답변이 일치하지 않습니다:(");
